97a9cf66e2f9fe9ddb500354e9f15dc3117bb041
RudraP272812 <rudrapatel2812.ca@gmail.com>
2025-09-11 13:44:25 -0400

    Lab 1

---
+++ src/main/java/info5153/exercises/server/employee/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package info5153.exercises.server.employee;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "employees", path = "employees")
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
